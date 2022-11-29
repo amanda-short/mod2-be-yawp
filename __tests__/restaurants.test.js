0b6341197c23d3efdde58c10997bbd736d57351c
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-11-28 17:25:13 -0800

    GET all with reviews and details passing

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -23,10 +23,48 @@ describe('restaurants route', () => {
   it('GET /api/v1/restaurants should return a list of restaurants', async () => {
     const resp = await request(app).get('/api/v1/restaurants');
     expect(resp.status).toBe(200);
-    expect(resp.body.length).toEqual((4), {
+    expect(resp.body.length).toEqual(4, {
       id: expect.any(String),
       name: expect.any(String),
     });
   });
 
+  it('GET /api/v1/restaurants/:restId should return a detailed list of restaurants with nested reviews', async () => {
+    const resp = await request(app).get('/api/v1/restaurants/1');
+    console.log(resp.body, 'restaurants!');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Object {
+        "cost": 1,
+        "cuisine": "American",
+        "id": "1",
+        "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+        "name": "Pip's Original",
+        "reviews": Array [
+          Object {
+            "detail": "Best restaurant ever!",
+            "id": "1",
+            "restaurant_id": "1",
+            "stars": 5,
+            "user_id": "1",
+          },
+          Object {
+            "detail": "Terrible service :(",
+            "id": "2",
+            "restaurant_id": "1",
+            "stars": 1,
+            "user_id": "2",
+          },
+          Object {
+            "detail": "It was fine.",
+            "id": "3",
+            "restaurant_id": "1",
+            "stars": 4,
+            "user_id": "3",
+          },
+        ],
+        "website": "http://www.PipsOriginal.com",
+      }
+    `);
+  });
 });
